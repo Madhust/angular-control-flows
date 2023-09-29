0ffc78f99a352f7dab6dec304db323342ace1540
Madhust <madhusudhananit@gmail.com>
2023-09-29 20:44:35 +0530

    Sample added

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+.catch(err => console.error(err));
+// bootstrapApplication(AppComponent)
+//   .catch(err => console.error(err));
